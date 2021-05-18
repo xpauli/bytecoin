3f94365ea649ed47fa85a4c57d3fbd9d41247312
Paul Popa <paul.popa30@gmail.com>
2021-05-18 11:50:55 +0300

    final version

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Paul Popa on 05.05.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
+
